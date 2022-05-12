c2fb53c572dfd1de60385b6804999eb19535d502
Tamer Berat Çelik <aducat5@hotmail.com>
2022-05-12 11:11:29 +0300

    added bootstrap table as component

---
+++ fileviewer-web-client/fileviewer-web-client/src/app/bs-table/bs-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bs-table',
+  templateUrl: './bs-table.component.html',
+  styleUrls: ['./bs-table.component.css']
+})
+export class BsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
