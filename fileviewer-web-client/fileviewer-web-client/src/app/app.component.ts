c8e145c1b208677c983d8071b4ac5af22b4b825a
Tamer Berat Çelik <aducat5@hotmail.com>
2022-05-13 09:57:42 +0300

    comments removed

--- fileviewer-web-client/fileviewer-web-client/src/app/app.component.ts
+++ fileviewer-web-client/fileviewer-web-client/src/app/app.component.ts
@@ -32,21 +32,16 @@ export class AppComponent {
       for (let i = 0; i < files.length; i++) {
         const file = files[i];
         let fileToUpload = new StoredFile(file);
-        
-        //do file reading here
         let reader = new FileReader();
         reader.onload = () => {
           fileToUpload.data = reader.result?.toString() || "";
           this.fileService.upload(fileToUpload).subscribe((data : any) => {
-            // this.groupedList = [...this.groupedList, fileToUpload];
             this.getAll(this._fileService);
             this.uploadedFiles = [...this.uploadedFiles, fileToUpload.name];
-            
           });
         };
         reader.readAsDataURL(file);
       }
-      
     }else{
       console.log("error");
     }
